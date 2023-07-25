3ea908ba0bf091b3b9f950f46292bd281e543a68
Mohamed A Elbou <medelbou@gmail.com>
2023-07-24 21:01:18 -0500

    default to the GA ID and Maps API key of UCB

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,4 +1,6 @@
 export const isUCB = document.location.hostname === 'waamd.lib.berkeley.edu';
 export const GOOGLE_ANALYTICS_ACCOUNT_ID =
-    process.env.REACT_APP_GOOGLE_ANALYTICS_ACCOUNT_ID;
-export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+    process.env.REACT_APP_GOOGLE_ANALYTICS_ACCOUNT_ID || 'G-3H4T9L018E';
+export const GOOGLE_MAPS_API_KEY =
+    process.env.REACT_APP_GOOGLE_MAPS_API_KEY ||
+    'AIzaSyCqhptLZq6kAnJw4I8z1FS5WF6rAbDtJzU';
